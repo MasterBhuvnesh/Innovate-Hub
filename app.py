744db903fc4ce9d3a89eacb2d4edd76e321e0e07
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-02-16 02:38:51 +0530

    modify app.py file

--- app.py
+++ app.py
@@ -6,13 +6,20 @@ app = Flask(__name__)
 with open('data.json', 'r', encoding='utf-8') as file:
     platforms = json.load(file)
 
+with open('categories.json', 'r', encoding='utf-8') as file:
+    categories = json.load(file)
+
 @app.route('/')
 def index():
-    return render_template('index.html', platforms=platforms, title="Innovate Hub")
+    return render_template('index.html', platforms=platforms, categories=categories, title="Innovate Hub")
 
 @app.route('/api', methods=['GET'])
 def get_platforms():
     return jsonify(platforms)
+
+@app.route('/categories', methods=['GET'])
+def get_platforms():
+    return jsonify(categories)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
