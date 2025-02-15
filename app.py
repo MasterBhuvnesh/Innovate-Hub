a4d58f5d3b0f191d835b7b7cc7279add3c881ec6
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2025-02-16 02:18:46 +0530

    modify & added api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 
 app = Flask(__name__)
@@ -9,6 +9,10 @@ with open('data.json', 'r', encoding='utf-8') as file:
 @app.route('/')
 def index():
     return render_template('index.html', platforms=platforms, title="Innovate Hub")
+
+@app.route('/api', methods=['GET'])
+def get_platforms():
+    return jsonify(platforms)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
